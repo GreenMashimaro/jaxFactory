a7aae145282cae6208d40f8e81566a98af6747db
greenmashimaro <greenmashimaro@gmail.com>
2022-03-02 19:00:00 +0800

    update

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ yarn install
 
 yarn build
 ```
+
+
+Related documents:
+
+English：https://www.typescriptlang.org/docs/handbook/jsx.html
+
+Chinese：https://www.tslang.cn/docs/handbook/jsx.html
--- src/components/jsxFactory.ts
+++ src/components/jsxFactory.ts
@@ -1,13 +1,1 @@
-export type CreateElementFn = (tagName: string, props: { [propName: string]: any } | null, ...children: any[]) => Element | DocumentFragment;
-
-export interface CustomFactoryConfig {
-    createElement: CreateElementFn
-}
-
-export const CUSTOM_FACTORY_CONFIGURATION: CustomFactoryConfig = {
-    createElement: () => {
-        throw Error('Please set an default jsx factory createElement, CUSTOM_FACTORY_CONFIGURATION.createElement = React.createElement')
-    }
-}
-
-export const customFactory: CreateElementFn = CUSTOM_FACTORY_CONFIGURATION.createElement;
+export const customFactory = () => {}
